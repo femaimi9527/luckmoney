9134ae3ac9c668a5561ae87fd4b66e05683ae968
femaimi <femaimi9527@gmail.com>
2019-04-18 01:53:11 +0800

    xxm 提交V-1.5

--- src/main/java/com/imooc/luckmoney/LuckmoneyController.java
+++ src/main/java/com/imooc/luckmoney/LuckmoneyController.java
@@ -2,8 +2,10 @@ package com.imooc.luckmoney;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -24,4 +26,23 @@ public class LuckmoneyController {
     public List<Luckmoney> list(){
         return repository.findAll();
     }
+
+    /**
+     * 创建红包（发红包）
+     * */
+    @GetMapping("/luckmoneys")
+    public Luckmoney create(@RequestParam("producer") String producer,
+                            @RequestParam("money") BigDecimal money){
+        Luckmoney luckmoney = new Luckmoney();
+        luckmoney.setProducer(producer);
+        luckmoney.setMoney(money);
+        return repository.save(luckmoney);
+    }
+    /**
+     * 通过id查询红包
+     * */
+
+    /**
+     * 更新红包（领红包）
+     * */
 }
